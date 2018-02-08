3b6c7e96ffe9d24e883cc4141ebf4fb3f92b8890
alexandrawaite <alexandrawaite@gmail.com>
2018-02-08 13:50:01 -0800

    Include start script in package.json

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,7 +1,7 @@
 const db = require('../db/db')
-const { encryptPassword } = require('../utilities/password')
+const { encryptPassword, comparePassword } = require('../utilities/password')
 
-const create = (user) => {
+const signUp = (user) => {
   return encryptPassword(user.password)
     .then((hashedPassword) => {
       const query = `
@@ -11,10 +11,33 @@ const create = (user) => {
           ($/name/, $/email/, $/password/, $/primary_city/)
         RETURNING *
       `
-      return db.one(query, user)
+      return db.one(query, user.name, user.email, hashedPassword, user.primary_city)
     })
 }
 
+const signIn = (user) => {
+  const query = `
+    SELECT * FROM
+      users
+    WHERE email = $/email/
+  `
+  return db.oneOrNone(query, user)
+  .then((queriedUser) => {
+    console.log('queriedUser.password =======>', queriedUser.password)
+    console.log('user.password =======>', user.password)
+    return comparePassword(user.password, queriedUser.password)
+      .then((validPassword) => {
+        console.log('validPass:::::', validPassword)
+        if (!validPassword) throw Error
+        return queriedUser
+      })
+  })
+  .catch((error) => {
+    console.log('Error occurred while executing signIn::', error)
+  })
+}
+
 module.exports = {
-  create
+  signUp,
+  signIn
 }
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,19 +1,33 @@
 const express = require('express')
-const user = require('../actions/user')
+const { signUp, signIn } = require('../actions/user')
 
 const router = express.Router()
 
 router.route('/sign-up')
-  .get((req, res) => {
-    res.render('authentication/sign-up', {error: ''})
-  })
-  .post((req, res, next) => {
-    user.create(req.body)
+  .get((req, res) => res.render('authentication/sign-up', { error: '' }))
+  .post((req, res) => {
+    signUp(req.body)
       .then(() => {
         res.redirect('/')
       })
       .catch(() => {
-        res.render('authentication/sign-up', {error: 'That email already exists'})
+        res.render('authentication/sign-up', { error: 'That email already exists' })
+      })
+  })
+
+router.route('/sign-in')
+  .get((req, res) => res.render('authentication/sign-in', { error: '' }))
+  .post((req, res) => {
+    signIn(req.body)
+      .then((user) => {
+        if (!user) {
+          res.render('authentication/sign-in', { error: 'Invalid username or password' })
+        }
+        req.session.user = user
+        res.redirect('/')
+      })
+      .catch(() => {
+        res.render('authentication/sign-in', { error: 'Invalid username or password' })
       })
   })
 
