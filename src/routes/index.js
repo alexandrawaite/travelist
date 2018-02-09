0ed05fe25128f46287dded355a06abea13ed2f10
alexandrawaite <alexandrawaite@gmail.com>
2018-02-09 00:25:19 -0800

    Add getCities function to render cities on the splash page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const auth = require('./authentication')
+const cities = require('./cities')
+const users = require('./users')
+const { getCities } = require('../actions')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => res.render('index'))
+routes.get('/', (req, res) => {
+  getCities()
+    .then((cities) => {
+      res.render('index', { cities })
+    })
+})
+
 routes.use('/', auth)
+routes.use('/cities', cities)
+routes.use('/users', users)
 
 module.exports = routes
