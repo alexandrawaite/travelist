f68d25bc5ba624de0f191b62b2c4c96d22b81b26
alexandrawaite <alexandrawaite@gmail.com>
2018-02-09 16:17:03 -0800

    Write routes to render private profile and edit profile

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,48 @@
 const express = require('express')
-const { getUserById } = require('../actions')
+const { getUserById, getPostsByUserId, updateUser } = require('../actions')
 
 const router = express.Router()
+
+router.get('/private', (req, res) => {
+    const { user } = req.session
+    if (!req.session.user) {
+      res.redirect('/sign-in')
+    }
+    getUserById(user.id)
+      .then((user) => {
+        getPostsByUserId(user.id)
+          .then((posts) => {
+            res.render('users/private-profile', { user, posts })
+          })
+      })
+})
 
 router.get('/:userId', (req, res) => {
   const { userId } = req.params
   getUserById(userId)
     .then((user) => {
-      res.render('users/profile', { user })
+      getPostsByUserId(userId)
+        .then((posts) => {
+          res.render('users/private-profile', { user, posts })
+        })
     })
 })
+
+router.get('/:userId/edit', (req, res) => {
+  const { userId } = req.params
+  getUserById(userId)
+    .then((user) => {
+      res.render('users/edit-form', { user })
+    })
+})
+
+router.put('/:userId', (req, res) => {
+  const { userId } = req.params
+  updateUser(userId, req.body)
+    .then(() => {
+      res.redirect(`/users/${userId}`)
+    })
+})
+
 
 module.exports = router
