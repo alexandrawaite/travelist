9689e972de50b0ac6a7462c820a90b337e07c955
alexandrawaite <alexandrawaite@gmail.com>
2018-02-09 16:14:56 -0800

    Set up middleware for post routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const auth = require('./authentication')
 const cities = require('./cities')
 const users = require('./users')
+const posts = require('./posts')
 const { getCities } = require('../actions')
+const { setDefaultResponseLocals, isLoggedIn } = require('./sessionMiddleware')
 
 const routes = express.Router()
+
+routes.use(setDefaultResponseLocals)
+routes.use(isLoggedIn)
 
 routes.get('/', (req, res) => {
   getCities()
@@ -16,5 +21,6 @@ routes.get('/', (req, res) => {
 routes.use('/', auth)
 routes.use('/cities', cities)
 routes.use('/users', users)
+routes.use('/posts', posts)
 
 module.exports = routes
