ab432a73a8e7b423bd9e1e6dc3854637004ef384
alexandrawaite <alexandrawaite@gmail.com>
2018-02-09 16:18:03 -0800

    Set up method-override middleware

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const session = require('express-session')
+const methodOverride = require('method-override')
 
 const routes = require('./routes')
 
@@ -16,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(`${ROOT_DIR}/public`))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(methodOverride('_method'))
 
 app.use(
   session({
@@ -23,11 +25,15 @@ app.use(
     secret: 'wooohooo',
     resave: false,
     saveUninitialized: false,
-    cookie: { expires: 600000 },
+    cookie: { maxAge: 600000 },
   })
 )
 
 app.use(routes)
+
+app.use((req, res) => {
+  res.status(404).render('common/not-found')
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}...`)
