c48ba74c06f6b9f86573da92f213041b281924a8
alexandrawaite <alexandrawaite@gmail.com>
2018-02-10 13:30:17 -0800

    Start routes for posts

---
+++ src/routes/posts.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const { createPost, getPostById, updatePost, getUserById, getCityById } = require('../actions')
+
+const router = express.Router()
+
+router.route('/:cityId/new')
+  .get((req, res) => {
+    const { user } = req.session
+    const { cityId } = req.params
+    getUserById(user.id)
+      .then((user) => {
+        getCityById(cityId)
+          .then((city) => {
+            res.render('posts/new', { user, city })
+          })
+      })
+  })
+  .post((req, res) => {
+    console.log('req.body:::', req.body)
+    createPost(req.body)
+      .then(() => {
+        res.redirect('/')
+      })
+  })
+
+router.get('/:postId', (req, res) => {
+  const { postId } = req.params
+  getPostById(postId)
+    .then((post) => {
+      res.render('posts/post', { post })
+    })
+})
+
+router.get('/:postId/edit', (req, res) => {
+  const { postId } = req.params
+  getPostById(postId)
+    .then((post) => {
+      res.render('posts/edit-form', { post })
+    })
+})
+
+router.put('/:postId', (req, res) => {
+  const { postId } = req.params
+  updatePost(postId, req.body)
+    .then(() => {
+      res.redirect('/')
+    })
+})
+
+module.exports = router
