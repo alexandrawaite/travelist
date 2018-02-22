8507eb679bf7c0f38f1632f9c8e723fc02026114
alexandrawaite <alexandrawaite@gmail.com>
2018-02-21 23:00:32 -0800

    Update seed data and schema with default values

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -1,15 +1,17 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS posts;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL,
   joined_at DATE DEFAULT CURRENT_DATE,
-  user_image_url TEXT,
+  user_image_url TEXT DEFAULT 'https://s18.postimg.org/5s07cv2ih/default-employer-profile.png',
   primary_city TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS cities;
 CREATE TABLE cities (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL UNIQUE,
@@ -17,7 +19,6 @@ CREATE TABLE cities (
   city_image_url TEXT
 );
 
-DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
   city_id INTEGER NOT NULL REFERENCES cities,
